b4cfea3f271253acd2ebd87ab5b74cd478c16ec1
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-03-22 11:59:02 -0600

    loggoff

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import './styles.css';
 import logo from '../../../public/Images/music-note.png';
 import Artistas from '../../../public/Images/Artistas.png';
 import Canciones from '../../../public/Images/Canciones.png';
 import Albumes from '../../../public/Images/Álbumes.png';
 import Reportes from '../../../public/Images/Reportes.png';
-
+import Logoff from '../../../public/Images/Logoff.png';
+import * as actions from '../../actions/loggedUser';
 
 const sidebarOptions = ['Canciones', 'Artistas', 'Álbumes', 'Reportes'];
 const optImg = [Canciones, Artistas, Albumes, Reportes];
-
-export const Sidebar = () => {
+let logoffimg = Logoff;
+const DummySidebar = ({loggoff}) => {
   document.body.style.backgroundColor = '#A8FFF5';
   return (
     <div className="sidebar">
@@ -30,7 +31,24 @@ export const Sidebar = () => {
             </div>
           </Link>
         ))}
+        <div className="sidebar-option" style={{paddingBottom:'1.5px'}}>
+          <Link  to={'/login' } style={{textDecoration:'none',color:'white'}} onClick={()=>loggoff()}>
+            <img alt="img" src={logoffimg} className="sidebar-image"/>
+             Cerrar sesión
+          </Link> 
+        </div>
       </div>
+   
     </div>
   );
 } 
+
+export const Sidebar = connect(
+  undefined,
+  (dispatch,state) => ({
+    loggoff() {
+      dispatch(actions.logout())
+      
+    },
+  }),
+)(DummySidebar);
